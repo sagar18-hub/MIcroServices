a5fe116a6f62266ded8b5a77e95c03a4194d8d40
abc <abc@example.com>
2020-01-17 16:13:18 +0530

    MIcroservices 3

---
+++ Ratings-Data-Service/src/test/java/com/catalog/Ratings/Data/Service/RatingsDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalog.Ratings.Data.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
