62c79595e9fe0e3bc7f1a2ca86077cf341880697
HP <HP@DESKTOP-QBCNUKF>
2020-02-04 22:30:10 +0530

    patient microservice

---
+++ PatientMicroService/src/main/java/com/example/PatientMicroService/PatientMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.PatientMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientMicroServiceApplication.class, args);
+	}
+
+}
---
+++ PatientMicroService/src/test/java/com/example/PatientMicroService/PatientMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PatientMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
